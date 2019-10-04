11e3b5381c5fe296a46fbc26520b188ba419bbc3
sonypiay <sonypiay@mail.com>
2019-10-04 17:27:50 +0700

    commit change

---
+++ app/Http/Controllers/ControlPanel/Pages/AdminOwnerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ControlPanel\Pages;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminOwnerController extends Controller
+{
+    //
+}

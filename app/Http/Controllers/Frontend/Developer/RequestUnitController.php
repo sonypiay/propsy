92375cfda9d886ee2cd2f62e8b16d57a4bd5662b
sonypiay <sonypiay@mail.com>
2020-01-15 15:01:58 +0700

    update request unit

--- app/Http/Controllers/Frontend/Developer/RequestUnitController.php
+++ app/Http/Controllers/Frontend/Developer/RequestUnitController.php
@@ -64,10 +64,10 @@ class RequestUnitController extends Controller
       'price_installment.installment_tenor'
     )
     ->join('customer', 'project_request.customer_id', '=', 'customer.customer_id')
-    ->join('city', 'customer.city_id', '=', 'city.city_id')
-    ->join('province', 'city.province_id', '=', 'province.province_id')
     ->join('project_unit_type', 'project_request.unit_type_id', '=', 'project_unit_type.unit_type_id')
     ->join('price_installment', 'project_request.installment', '=', 'price_installment.id')
+    ->leftJoin('city', 'customer.city_id', '=', 'city.city_id')
+    ->leftJoin('province', 'city.province_id', '=', 'province.province_id')
     ->leftJoin('meeting_appointment', 'project_request.request_id', '=', 'meeting_appointment.request_id');
 
     if( empty( $keywords ) )
